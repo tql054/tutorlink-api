a0ba86a8d7168cf5b396dc983482aa36b36c542f
tql054 <tuanquocle2001@gmail.com>
2023-05-28 10:55:20 +0700

    commit fixing get current date function #5

--- src/services/index.js
+++ src/services/index.js
@@ -6,7 +6,7 @@ const getCurrentDatetime = () => {
     let currentMonth = currentdate.getMonth() + 1
     if(currentDay< 10) currentDay = `0${currentDay}`
     if(currentMonth< 10) currentMonth = `0${currentMonth}`
-    return currentdate.getFullYear() + "-" + currentMonth + "-" + currentDay + " " + `${currentdate.getHours}:${currentdate.getMinutes}:00`
+    return currentdate.getFullYear() + "-" + currentMonth + "-" + currentDay + " " + `${currentdate.getHours()}:${currentdate.getMinutes()}:00`
 }
 
 export default getCurrentDatetime
